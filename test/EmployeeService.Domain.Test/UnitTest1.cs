87517b8096ea081fe8bd25f322edaebe9e300ed8
Aleazar yilma <aleazaryilma@gmail.com>
2023-04-24 18:52:50 +0300

    setup test projects

---
+++ test/EmployeeService.Domain.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EmployeeService.Domain.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
