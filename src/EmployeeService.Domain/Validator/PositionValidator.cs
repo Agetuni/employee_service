1fdade1fb7fe5ef294664f0bacbc157491b6058b
Aleazar yilma <aleazaryilma@gmail.com>
2023-04-24 18:54:30 +0300

    add validator for position model

---
+++ src/EmployeeService.Domain/Validator/PositionValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeService.Domain.Validator;
+public class PositionValidator : AbstractValidator<Position>
+{
+    public PositionValidator()
+    {
+
+        RuleFor(x => x.Name)
+              .NotNull().WithMessage("Position name can't be null")
+              .NotEmpty().WithMessage("Position name can't be empty");
+    }
+
+}
+
