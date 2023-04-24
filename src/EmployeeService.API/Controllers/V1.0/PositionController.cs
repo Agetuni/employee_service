eb1b4396d98b92915307a22418bdd83e5ec782c1
Aleazar yilma <aleazaryilma@gmail.com>
2023-04-24 18:58:13 +0300

    add inital controller

---
+++ src/EmployeeService.API/Controllers/V1.0/PositionController.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeService.API.Resources;
+using Microsoft.Extensions.Localization;
+
+namespace EmployeeService.API.Controllers.V1._0;
+public class PositionController : BaseController
+{
+    [HttpPost("Create")]
+    public async Task<IActionResult> CreateClient([FromBody] CreatePositionDto request)
+    {
+        
+        var result = await _mediator.Send(new CreatePosition { Name = request.Name });
+        var client = _mapper.Map<PositionDetailDto>(result.Payload);
+        return result.IsError ? HandleErrorResponse(result.Errors) : Ok(client);
+     
+    }
+}
