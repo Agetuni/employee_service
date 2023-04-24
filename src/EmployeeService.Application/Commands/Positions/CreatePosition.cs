8ea08cccfcdd28879e1107e3ec0e43b7da88b2ec
Aleazar yilma <aleazaryilma@gmail.com>
2023-04-24 18:56:37 +0300

    add cqrs folders

---
+++ src/EmployeeService.Application/Commands/Positions/CreatePosition.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeService.Application.Commands.Positions;
+public class CreatePosition : IRequest<OperationResult<Position>>
+{
+    public string Name { get; set; }
+}
+public class CreatePositionHandler : IRequestHandler<CreatePosition, OperationResult<Position>>
+{
+    private readonly IRepositoryBase<Position> _position;
+    public CreatePositionHandler(IRepositoryBase<Position> _position) => this._position = _position;
+    public async Task<OperationResult<Position>> Handle(CreatePosition request, CancellationToken cancellationToken)
+    {
+        var result = new OperationResult<Position>();
+        try
+        {  
+            if (_position.ExistWhere(x => x.Name == request.Name && x.RecordStatus != Domain.Common.RecordStatus.Deleted))
+            {
+                result.AddError(ErrorCode.RecordAlreadyExists, "Record already exists.");
+                return result;
+            }
+            var position = Position.Create(request.Name);
+            await _position.AddAsync(position);
+
+            result.Payload = position;
+            result.Message = "Operation success";
+            return result;
+        }
+        catch (ValidationException ex) 
+        { 
+
+        }
+        return result;
+    }
+}
---
+++ src/EmployeeService.Application/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using MediatR;
+global using System.Linq.Expressions;
+global using EmployeeService.Domain.Models;
+global using Microsoft.EntityFrameworkCore;
+global using EmployeeService.Application.Models;
+global using EmployeeService.Infrastructure.Context;
+global using EmployeeService.Application.Services.GenericRepository;
