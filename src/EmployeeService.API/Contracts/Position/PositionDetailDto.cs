95ff702c13354766d257d7109dda71895d393243
Aleazar yilma <aleazaryilma@gmail.com>
2023-04-25 19:45:59 +0300

    add position dto

--- src/EmployeeService.API/Contracts/Position/PositionDetailDto.cs
+++ src/EmployeeService.API/Contracts/Position/PositionDetailDto.cs
@@ -1,6 +1,11 @@
-﻿namespace EmployeeService.API.Contracts.Positions;
+﻿using EmployeeService.Domain.Common;
+
+namespace EmployeeService.API.Contracts.Positions;
 
 public class PositionDetailDto
 {
+    public long Id { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
+    public RecordStatus RecordStatus { get; set; }
 }
---
+++ src/EmployeeService.API/Contracts/Position/PositionDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeService.API.Contracts.Positions;
+public class PositionDto
+{
+    [Required(ErrorMessage = "Position name is mandatory.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Position description is mandatory.")]
+    public string Description { get; set; }
+}
