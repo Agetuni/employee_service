6968a6d885542c54d26a59eae08cd84e80c705c3
Aleazar yilma <aleazaryilma@gmail.com>
2023-04-25 19:45:49 +0300

    add record  status dto

---
+++ src/EmployeeService.API/Contracts/Common/RecordStatusDto.cs
@@ -0,0 +1,9 @@
+﻿using EmployeeService.Domain.Common;
+
+namespace EmployeeService.API.Contracts.Common
+{
+    public class RecordStatusDto
+    {
+        public RecordStatus Status { get; set; }
+    }
+}
